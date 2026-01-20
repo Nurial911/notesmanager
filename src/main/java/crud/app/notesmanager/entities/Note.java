0f36bf0d5d666a34b2f5a7b8ccc7219e1de43896
Nurial <nurali.manarbekov@nu.edu.kz>
2026-01-21 04:20:13 +0500

    feat: add Note entity

---
+++ src/main/java/crud/app/notesmanager/entities/Note.java
@@ -0,0 +1,32 @@
+package crud.app.notesmanager.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "created_at", insertable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
