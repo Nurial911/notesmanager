e33119ae593442c7be807960e66c00633bc705ce
Nurial <nurali.manarbekov@nu.edu.kz>
2026-01-22 01:19:37 +0500

    fix: resolve compilation error in AuthorServiceImpl

--- src/main/java/crud/app/notesmanager/services/impl/AuthorServiceImpl.java
+++ src/main/java/crud/app/notesmanager/services/impl/AuthorServiceImpl.java
@@ -1,11 +1,12 @@
 package crud.app.notesmanager.services.impl;
 
 import crud.app.notesmanager.repositories.AuthorRepository;
+import crud.app.notesmanager.services.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthorServiceImpl {
+public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
 }
