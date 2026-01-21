8a4f71b88c206e48e3ca64cacfb634f685adc193
Nurial <nurali.manarbekov@nu.edu.kz>
2026-01-21 07:41:23 +0500

    fix: add explicit setters/getters for id, createdAt and updatedAt

--- src/main/java/crud/app/notesmanager/entities/Note.java
+++ src/main/java/crud/app/notesmanager/entities/Note.java
@@ -28,5 +28,15 @@ public class Note {
     @Column(name = "updated_at")
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+
 }
 
