1283f00afa28d4713d2ee967b624d84dc83c8efb
Nurial <nurali.manarbekov@nu.edu.kz>
2026-01-22 04:41:41 +0500

    fix: add missing @Data in AuthorRequest and missing @Mapping in AuthorMapper

--- src/main/java/crud/app/notesmanager/dtos/AuthorRequest.java
+++ src/main/java/crud/app/notesmanager/dtos/AuthorRequest.java
@@ -1,5 +1,8 @@
 package crud.app.notesmanager.dtos;
 
+import lombok.Data;
+
+@Data
 public class AuthorRequest {
     private String name;
     private String email;
--- src/main/java/crud/app/notesmanager/mappers/AuthorMapper.java
+++ src/main/java/crud/app/notesmanager/mappers/AuthorMapper.java
@@ -4,10 +4,12 @@ import crud.app.notesmanager.dtos.AuthorRequest;
 import crud.app.notesmanager.dtos.AuthorResponse;
 import crud.app.notesmanager.entities.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
     AuthorResponse entityToDto(Author author);
 
+    @Mapping(target = "id", ignore = true)
     Author dtoToEntity(AuthorRequest authorRequest);
 }
